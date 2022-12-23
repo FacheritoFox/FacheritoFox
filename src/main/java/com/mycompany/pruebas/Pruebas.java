ee2b359ace64d41581d5ab24d3d2bd1de5a0f26b
jgarces <jgarces@DESKTOP-85L9C4O.ingeneo.local>
2022-12-23 15:16:10 -0500

    Esto es una prueba de subir a git

---
+++ src/main/java/com/mycompany/pruebas/Pruebas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.pruebas;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author jgarces
+ */
+public class Pruebas {
+
+    public static void main(String[] args) {
+       String hola = JOptionPane.showInputDialog("Ingresar nombre") ;
+        String jaja = JOptionPane.showInputDialog("Ingresar tu edad " + hola) ;
+        System.out.println("Prueba " + hola +" "+ jaja);
+    }
+}
+
+
+
+
+
+
